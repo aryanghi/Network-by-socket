b1cd6ba02cfa339102658aad13261c32175efb75
salam <aryangholami222@gamil.com>
2024-01-06 15:34:53 +0330

    edit commit

--- server.py
+++ server.py
@@ -1,22 +1,60 @@
 import socket
 import threading as th
 
-
-def handel_client(c,addr):
+def handel_client(c, addr):
     print(f'has connect from {addr}')
     while True:
-        order=''
+        order = b''
         while True:
-            st=c.recv(1)
-            if st==b'\n':
+            st = c.recv(1)
+            if not st:
+                break
+            if st == b'\n':
                 break
             else:
-                order += st.decode()
-        if order=="put file":
-            print("please put a file")
-        elif order == "exit":
+                order += st
+
+        if order == b"put file":
+            size = b''
+            name = b''
+            format = b''
+            while True:
+                st = c.recv(1)
+                if st == b'\n':
+                    break
+                else:
+                    size += st
+
+            print(f'The size is {size.decode("utf-8")}')
+            while True:
+                st = c.recv(1)
+                if st == b'\n':
+                    break
+                else:
+                    name += st
+
+            while True:
+                st = c.recv(1)
+                if st == b'\n':
+                    break
+                else:
+                    format += st
+
+            print('start to rev bytes')
+            bytes=b''
+            while True:
+                st=c.recv(1)
+                if not st:
+                    break
+                bytes += st
+
+            with open(name.decode()+'recv'+format.decode(),'wb') as f:
+                f.write(bytes)
+
+
+        elif order == b"exit":
             break
-        elif order == 'get file':
+        elif order == b'get file':
             pass
         else:
             pass
@@ -29,9 +67,7 @@ def main():
     s.listen(10)
     print('The server is listning')
     while True:
-        c , addr = s.accept()
-        th.Thread(target=handel_client , args=(c,addr)).start()
+        c, addr = s.accept()
+        th.Thread(target=handel_client, args=(c, addr)).start()
 
 main()
-
-
