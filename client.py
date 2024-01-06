3785be5d8151975cd9c25f9f2ab6458c1fe84767
salam <aryangholami222@gamil.com>
2024-01-06 15:38:02 +0330

    edit commit

--- client.py
+++ client.py
@@ -1,13 +1,22 @@
 import socket
 import os
 
-s=socket.socket()
-s.connect(('127.0.0.1',12345))
-print('the clinet is connected')
+s = socket.socket()
+s.connect(('127.0.0.1', 12345))
+print('the client is connected')
 
 def put(file_name):
     global s
-    s.send("put file".encode() + b'\n')
+    size = os.path.getsize(file_name)
+    file_name, file_format = os.path.split(file_name)
+    size = str(size)
+    s.send(b"put file\n")
+    s.send(size.encode('utf-8') + b'\n')
+    s.send(file_name.encode('utf-8') + b'\n')
+    s.send(file_format.encode('utf-8') + b'\n')
+    with open(file_name + file_format, 'rb') as f:
+        bytes = f.read()
+    s.send(bytes)
 
 def get(filename):
     pass
@@ -15,3 +24,4 @@ def get(filename):
 def disconnect():
     pass
 
+put('received_file.png')
